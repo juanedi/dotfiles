f2e2b2db40ea44df8b6fcf0922c807bdf632a93f
Juan Edi <jedi11235@gmail.com>
2017-05-07 02:58:47 -0400

    'ssht' function to log in and launch tmux

--- zsh/util_mac.sh
+++ zsh/util_mac.sh
@@ -1,21 +1,5 @@
-function pdfltx {
-  pdflatex $1.tex && rm -f *.aux *.log *.out *.nav *.snm *.toc && open $1.pdf
-}
-
-function pg {
-  nohup pgweb --host localhost --user jedi --db $1 --listen $2 >/dev/null 2>&1 &
-}
-
-function erl-test {
-  local TEST=$1
-  make
-  if [ "$?" = "0" ]; then
-    PATH_REGEX="*test/$TEST*.erl"
-    APP_DIR=$(find apps -path "$PATH_REGEX" | sed -E 's:(apps/[^/]+).*:\1:')
-    cd $APP_DIR
-    rebar eunit suites=$TEST
-    cd -
-  fi
+function ssht {
+  ssh $1 -t tmux
 }
 
 function ggrep {
@@ -24,4 +8,3 @@ function ggrep {
 
 # add hostname to prompt if connected through ssh
 export PROMPT="${SSH_TTY:+[%m] }$PROMPT"
-
