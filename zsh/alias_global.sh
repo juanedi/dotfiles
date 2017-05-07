98119c5aace95b95a1a3f458c86c690af79d5f37
Juan Edi <jedi11235@gmail.com>
2017-05-07 16:50:06 -0400

    Tidy up zsh init files

--- zsh/alias_global.sh
+++ zsh/alias_global.sh
@@ -7,16 +7,11 @@ alias "...."="cd ../../.."
 alias trim="sed \"s/^ *//g;s/ *$//g\""
 alias quote="sed 's/^/\"/g' | sed 's/$/\"/g'"
 
-alias jfind="find . | grep \.java$ | grep "
-alias jgrep="find . | grep -v target | grep java | xargs grep "
 alias igrep="grep -i"
 
 alias hgrep="history | grep -i"
-alias rgrep="grep -r"
 alias matchingfiles="cut -d ':' -f 1 | sort | uniq"
 
-# alias cleantex='svn st | cut -c 9- | grep -e \.aux$ -e \.log$ -e \.out$ -e \.nav$ -e \.snm$ -e \.toc$ | xargs rm'
-alias cleantex='git status | grep -v modified | grep -e \.aux$ -e \.log$ -e \.out$ -e \.nav$ -e \.snm$ -e \.toc$  | cut -c 2- | xargs rm'
 alias be='bundle exec'
 
 alias v='view'
--- zsh/util_mac.sh
+++ zsh/util_mac.sh
@@ -1,11 +1,18 @@
-function ssht {
-  ssh $1 -t tmux
+alias pi-music-down='rsync -rau --progress --delete --exclude=iTunes pi@10.0.1.13:/mnt/PASSPORT/music/ $HOME/Music'
+alias pi-music-up='rsync -rau --progress --exclude=iTunes --perms $HOME/Music/ pi@10.0.1.13:/mnt/PASSPORT/music'
+alias o='open .'
+alias lstcp='lsof -i -n -P | grep TCP | grep LISTEN'
+alias sshconfig="$EDITOR $HOME/.ssh/config"
+alias dm='docker-machine'
+alias dc='docker-compose'
+alias tmux-clear='tmux ls | grep -v attached | cut -d : -f 1 | xargs -n1 tmux kill-session -t'
+
+function dsh {
+    docker-compose run $1 /bin/bash
 }
 
-alias tmuxclear='tmux ls | grep -v attached | cut -d : -f 1 | xargs -n1 tmux kill-session -t'
-
-function ggrep {
-  git log --grep="$1"
+function ssht {
+  ssh $1 -t tmux
 }
 
 # add hostname to prompt if connected through ssh
