d4818003420fd17fdff7a4a059f1c9aa67f9f275
Juan Edi <jedi11235@gmail.com>
2017-06-26 21:15:12 -0300

    Fix music synx aliases

--- zsh/util_mac.sh
+++ zsh/util_mac.sh
@@ -1,5 +1,5 @@
-alias pi-music-down='rsync -rau --progress --delete --exclude=iTunes pi@10.0.1.13:/mnt/PASSPORT/music/ $HOME/Music'
-alias pi-music-up='rsync -rau --progress --exclude=iTunes --perms $HOME/Music/ pi@10.0.1.13:/mnt/PASSPORT/music'
+alias pi-music-down='rsync -rau --progress --delete --exclude=iTunes osmc@10.0.1.13:/media/samsung/music/ $HOME/Music'
+alias pi-music-up='rsync -rau --progress --exclude=iTunes --perms $HOME/Music/ osmc@10.0.1.13:/media/samsung/music'
 alias o='open .'
 alias lstcp='lsof -i -n -P | grep TCP | grep LISTEN'
 alias sshconfig="$EDITOR $HOME/.ssh/config"
