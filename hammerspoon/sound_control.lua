3db212d3e9cec920d0d414683239c79c51db8b60
Juan Edi <jedi@Juans-MacBook-Pro.local>
2021-05-23 11:11:36 -0300

    allow to skip certain devices when switching!

--- hammerspoon/sound_control.lua
+++ hammerspoon/sound_control.lua
@@ -1,10 +1,66 @@
 sound_control = {}
 
+function array_find(array, predicate)
+  for i,v in ipairs(array) do
+    if predicate(v) then
+      return i
+    end
+  end
+
+  return nil
+end
+
+function array_select_first(array, predicate)
+  for i,v in ipairs(array) do
+    if predicate(v) then
+      return v
+    end
+  end
+
+  return nil
+end
+
+function array_contains(array, value)
+  for _,v in ipairs(array) do
+    if value == v then
+      return true
+    end
+  end
+
+  return false
+end
+
+function array_from_range(i, j)
+  result = {}
+
+  for x=i,j do
+    table.insert(result, x)
+  end
+
+  return result
+end
+
+function array_concat(array1, array2)
+  result = {}
+
+  for _,v in ipairs(array1) do
+    table.insert(result, v)
+  end
+
+  for _,v in ipairs(array2) do
+    table.insert(result, v)
+  end
+
+  return result
+end
+
 local device_display_names = {}
 device_display_names["Audioengine 2+  "] = "A2+"
 device_display_names["CalDigit Thunderbolt 3 Audio"] = "TS3+"
 device_display_names["External Headphones"] = "Headphones"
 device_display_names["MacBook Pro Speakers"] = "MBP"
+
+local skipped_devices = {"DELL U2718Q"}
 
 local iconAscii = [[ASCII:
 · · · · · · · · · · · ·
@@ -25,19 +81,23 @@ function sound_control.nextOutput()
   local devices = hs.audiodevice.allOutputDevices()
   local current_device = hs.audiodevice.current().uid
 
-  local next_device = nil
+  current_device_index = array_find(devices, function(device)
+    return device:uid() == current_device
+  end)
 
-  for i, device in pairs(devices) do
-    if device:uid() == current_device then
-      if devices[i+1] ~= nil then
-        next_device = devices[i+1]
-      else
-        next_device = devices[1]
-      end
-    end
+  local devices_to_try = array_concat(
+    array_from_range(current_device_index + 1, rawlen(devices)),
+    array_from_range(1, current_device_index - 1)
+  )
+
+  local next_device_index = array_select_first(devices_to_try, function(other_device_index)
+    other_device = devices[other_device_index]
+    return not array_contains(skipped_devices, other_device:name())
+  end)
+
+  if next_device_index ~= nil then
+    devices[next_device_index]:setDefaultOutputDevice()
   end
-
-  next_device:setDefaultOutputDevice()
 end
 
 function sound_control:init_menubar()
