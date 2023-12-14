c83432e45ac48ee445404d00e57cfeceb1e0c2f3
Juan Edi <juan@noredink.com>
2023-12-14 10:27:44 -0300

    hacky zellij integration

--- zsh/zellij-integration.zsh
+++ zsh/zellij-integration.zsh
@@ -1,5 +1,7 @@
 if [[ ! -f "$HOME/.nozellij" && -z "$ZELLIJ" ]]; then
-    if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+    if [[ "$ITERM_PROFILE" == "NRI Services" ]]; then
+        zellij a nri-services || zellij --layout nri -s nri-services
+    elif [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
         zellij attach -c
     else
         zellij
